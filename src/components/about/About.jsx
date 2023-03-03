abae364c3a40ec4fa2f53333d4d71ca47aa20996
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-03-03 15:08:02 +0545

    working on about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import ME from "../../assets/about-me.png";
 
 function About() {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know Me</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about_contents">
+          <div className="about_cards">
+            <article className="about_card">
+              <h5>Experience</h5>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
