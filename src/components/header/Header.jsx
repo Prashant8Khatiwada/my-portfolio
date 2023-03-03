fa6dfac12e5a7d6ae2ceae6b418c2747025fd1f6
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-03-03 13:36:54 +0545

    basic

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
 function Header() {
-  return <div color="blue">Header</div>;
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Prashant Khatiwada</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+function HeaderSocials() {
+  return (
+    <div className="header_socials">
+      <a href="https://linked.com" target="_blank"></a>
+      <a href="https://github.com" target="_blank"></a>
+      <a href="https://twitter.com" target="_blank"></a>
+    </div>
+  );
+}
+
+export default HeaderSocials;
