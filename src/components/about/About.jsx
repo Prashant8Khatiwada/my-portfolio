580604b250359b03486b7440d582c14d1313ddf5
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-03-04 16:29:09 +0545

    about section completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,8 @@
-import ME from "../../assets/about-me.png";
+import ME from "../../assets/about -me.png";
+import { FaAward } from "react-icons/fa";
+import { FiUser } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+import "./about.css";
 
 function About() {
   return (
@@ -16,9 +20,35 @@ function About() {
         <div className="about_contents">
           <div className="about_cards">
             <article className="about_card">
+              <FaAward className="about_icon" />
               <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about_card">
+              <FiUser className="about_icon" />
+              <h5>Clients</h5>
+              <small>50+ WorldWide</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>50+ Completed</small>
             </article>
           </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
+            distinctio id rem quae aliquid! Sapiente rerum accusantium cumque
+            molestiae provident vero quod ipsum, assumenda, modi autem iste
+            obcaecati voluptates sit eveniet nulla amet, iure earum asperiores
+            impedit natus facilis? Voluptas a tempore vel ratione ipsum fugit
+            omnis minus optio nostrum!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
