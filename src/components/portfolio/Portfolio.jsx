b6e765ec39d9279fbe860521946b76d97d60c2e1
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-05-10 22:06:31 +0545

    mediaquery added to portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,56 @@ import IMG1 from "../../assets/project-1.png";
 import IMG2 from "../../assets/project-2.png";
 import IMG3 from "../../assets/project-3.png";
 import IMG4 from "../../assets/project-4.png";
+
+const data = [
+  {
+    id: 1,
+    Image: IMG1,
+    title: "Chat GPT",
+    github: "https://github.com/Prashant8Khatiwada/Chat-GPT",
+    demo: "https://pk-chat-gpt.netlify.app/",
+  },
+  {
+    id: 2,
+    Image: IMG2,
+    title: "Youtube Clone",
+    github: "https://github.com/Prashant8Khatiwada/youtube-app",
+    demo: "https://p-youtube-clone.netlify.app",
+  },
+  {
+    id: 3,
+    Image: IMG3,
+    title: "Jokes Generator",
+    github:
+      "https://github.com/Prashant8Khatiwada/50-project-javascript/tree/main/09_dadJokes",
+    demo: "https://prashant8khatiwada.github.io/50-project-javascript/09_dadJokes",
+  },
+  {
+    id: 4,
+    Image: IMG4,
+    title: "Movie App",
+    github:
+      "https://github.com/Prashant8Khatiwada/50-project-javascript/tree/main/12_movieApp",
+    demo: "https://prashant8khatiwada.github.io/50-project-javascript/12_movieApp/",
+  },
+  {
+    id: 1,
+    Image: IMG1,
+    title: "Chat GPT",
+    github:
+      "https://github.com/Prashant8Khatiwada/50-project-javascript/tree/main/12_movieApp",
+    demo: "https://prashant8khatiwada.github.io/50-project-javascript/12_movieApp/",
+  },
+  {
+    id: 1,
+    Image: IMG4,
+    title: "Chat GPT",
+    github:
+      "https://github.com/Prashant8Khatiwada/50-project-javascript/tree/main/12_movieApp",
+    demo: "https://prashant8khatiwada.github.io/50-project-javascript/12_movieApp/",
+  },
+];
+
 function Portfolio() {
   return (
     <section id="portfolio">
@@ -11,145 +61,24 @@ function Portfolio() {
       <h2>Portfolio</h2>
 
       <div className="container portfolio_container">
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is portfolio title</h3>
-          <div className="portfolio_item-cta">
-          <a
-            href="https://github.com/Prashant8Khatiwada/Chat-GPT"
-            target="_blank"
-            className="btn"
-          >
-            Github
-          </a>
-          <a
-            href="https://pk-chat-gpt.netlify.app/"
-            target="_blank"
-            className="btn btn-primary"
-          >
-            Live Demo
-          </a>
-          </div>
-        </article>
-        {/* end of chat gpt  */}
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG2} alt="" />{" "}
-          </div>
-          <h3>This is portfolio title</h3>
-          <div className="portfolio_item-cta">
-          <a
-            href="https://github.com/Prashant8Khatiwada/youtube-app"
-            target="_blank"
-            className="btn"
-          >
-            Github
-          </a>
-          <a
-            href="https://p-youtube-clone.netlify.app"
-            target="_blank"
-            className="btn btn-primary"
-          >
-            Live Demo
-          </a>
-          </div>
-
-        </article>
-        {/* end of YouTube App */}
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG3} alt="" />{" "}
-          </div>
-          <h3>This is portfolio title</h3>
-          <div className="portfolio_item-cta">
-            <a
-              href="https://github.com/Prashant8Khatiwada/50-project-javascript/tree/main/09_dadJokes"
-              target="_blank"
-              className="btn"
-            >
-              Github
-            </a>
-            <a
-              href=" https://prashant8khatiwada.github.io/50-project-javascript/09_dadJokes"
-              target="_blank"
-              className="btn btn-primary"
-            >
-              Live Demo
-            </a>
-            </div>
-        </article>
-        {/* end of Dad Jokes  */}
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG4} alt="" />{" "}
-          </div>
-          <h3>This is portfolio title</h3>
-          <div className="portfolio_item-cta">
-          <a
-            href="https://github.com/Prashant8Khatiwada/50-project-javascript/tree/main/12_movieApp"
-            target="_blank"
-            className="btn"
-          >
-            Github
-          </a>
-          <a
-            href="https://prashant8khatiwada.github.io/50-project-javascript/12_movieApp/"
-            target="_blank"
-            className="btn btn-primary"
-          >
-            Live Demo
-          </a>
-          </div>
-        </article>
-        {/* end of chat gpt  */}
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG1} alt="" />{" "}
-          </div>
-          <h3>This is portfolio title</h3>
-          <div className="portfolio_item-cta">
-            <a
-              href="https://github.com/prashant8khatiwada"
-              target="_blank"
-              className="btn"
-            >
-              Github
-            </a>
-            <a
-              href="https://livedemo"
-              target="_blank"
-              className="btn btn-primary"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        {/* end of chat gpt  */}
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={IMG3} alt="" />{" "}
-          </div>
-          <h3>This is portfolio title</h3>
-          <div className="portfolio_item-cta">
-            <a
-              href="https://github.com/prashant8khatiwada"
-              target="_blank"
-              className="btn"
-            >
-              Github
-            </a>
-            <a
-              href="https://livedemo"
-              target="_blank"
-              className="btn btn-primary"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        {/* end of chat gpt  */}
+        {data.map(({ id, Image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio_item">
+              <div className="portfolio_item-image">
+                <img src={Image} alt="" />{" "}
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio_item-cta">
+                <a href={github} target="_blank" className="btn">
+                  Github
+                </a>
+                <a href={demo} target="_blank" className="btn btn-primary">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
